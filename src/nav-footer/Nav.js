1b5d7e92c23dac03fdfcaefa5446e7e330f27102
yohochen <sunny_chen628@hotmail.com>
2020-06-08 18:08:14 -0700

    hide nav bar

--- src/nav-footer/Nav.js
+++ src/nav-footer/Nav.js
@@ -6,7 +6,7 @@ export class Nav extends React.Component {
         return(
               <nav>
                 <div className="row">
-                  <ul className="main-nav">
+                  <ul id="navbar" className="main-nav">
                     <li><a href="#about">About</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#timeline">Timeline</a></li>
